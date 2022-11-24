b79fe9acfe5f63674a5d8791426b5a6d06056a69
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-11-24 15:26:00 +0300

    Ex003 added

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿// Создать программу сложения двух целых чисел с выведением суммы
+int numberA = 31;
+int numberB = 54;
+int result = numberA + numberB;
+Console.Write("Sum = ");
+Console.WriteLine(result);
