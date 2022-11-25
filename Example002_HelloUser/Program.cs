3528ea52a2c205765e21755185733be07d4bee33
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-11-25 15:26:57 +0300

    bug fix

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,6 +1,12 @@
-﻿//Комментарий 2
-Console.WriteLine("Введиет имя пользователя ");
-string username = Console.ReadLine();
-Console.Write("Hello, ");
-Console.Write(username);
-Console.WriteLine("!");
+﻿int patient = 1;
+int doctor = 0;
+int help = patient * doctor;
+
+if(help == 0)
+{
+    Console.WriteLine("Press 'F' to Respect");
+}
+else
+{
+    Console.WriteLine("Miracle!!!");    
+}
