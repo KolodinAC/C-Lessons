0a7bc98e960b3af54b5e07ed85b86a86e6a435ac
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-11-25 09:58:36 +0300

    bug fix

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Комментарий 2
-Console.WriteLine("Enter your name");
+Console.WriteLine("Введиет имя пользователя ");
 string username = Console.ReadLine();
 Console.Write("Hello, ");
 Console.Write(username);
