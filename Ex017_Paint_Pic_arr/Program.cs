b5e0dc5af4ee7bac53c1254c0742480a93397f66
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-16 10:32:50 +0300

    ex 16 17 18 added

--- Ex017_Paint_Pic_arr/Program.cs
+++ Ex017_Paint_Pic_arr/Program.cs
@@ -1,33 +1,6 @@
 ﻿// Создадим алгоритм закраски черно белого изображения из массива:
 
-
-void PrintImage(int[,] image)           
-{
-    for (int i = 0; i < image.GetLength(0); i++)           
-    {
-        for (int j = 0; j < image.GetLength(1); j++)      
-        {  
-            if(image[i, j] == 0) Console.Write($" ");
-            else Console.Write($"+");
-        }
-        Console.WriteLine();
-    }
-}
-
-
-// void FillTwoDimArray(int[,] matr)
-// {
-//     for (int i = 0; i < matr.GetLength(0); i++)           
-//     {
-//         for (int j = 0; j < matr.GetLength(1); j++)      
-//         {
-//             matr[i, j] = new Random().Next(1, 10);
-//         }
-//     }
-// }
-
-
-int[,] pic = new int[,]
+int[,] pic = new int[,]                                                         // оюъявляем переменную pic содержащую двухмерный массив
 {
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -53,5 +26,57 @@ int[,] pic = new int[,]
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 };
-                 
+
+void PrintImage(int[,] image)           
+{
+    for (int i = 0; i < image.GetLength(0); i++)           
+    {
+        for (int j = 0; j < image.GetLength(1); j++)      
+        {  
+            if(image[i, j] == 0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillImage(int row, int col)   // алгоритм позволяет закршивать области замкнутые внутри массива
+{
+    if(pic[row, col] == 0)   // если точка с заданными координатами не закрашена, тоесть == 0 то запускается цикл закршивания:
+    {
+        pic[row, col] = 1;              // определяем переменную
+        FillImage(row - 1, col);        // идем от началной точки вверх и если она не закрашена единицей то красим.    Если метод или функция вызывает сам себя это называется рекурсия.
+        FillImage(row, col - 1);        // далее идем от закрашенной точки влева.    Если метод или функция вызывает сам себя это называется рекурсия.
+        FillImage(row + 1, col);        // далее вниз.    Если метод или функция вызывает сам себя это называется рекурсия.
+        FillImage(row, col + 1);       // далее вправо.    Если метод или функция вызывает сам себя это называется рекурсия.
+    }                                 // Если метод или функция вызывает сам себя это называется рекурсия.
+}
+
 PrintImage(pic);
+FillImage(13, 13);
+PrintImage(pic);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void FillTwoDimArray(int[,] matr)     // метод заполняет двумерный массив рандомными числами в диапазоне от min до max
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)           
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)      
+//         {
+//             matr[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+                 
