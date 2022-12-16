0fb215244012aaa51061ed0647b49d7978adff48
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-16 10:45:23 +0300

    task 18 added

---
+++ Ex018_Recursyy_Febonachi/Program.cs
@@ -0,0 +1,16 @@
+﻿// Создадим алгоритм вывода чисел Фебоначи через рекурсию (слудеющее число является суммой двух предидущих чисел от 1 до заданного числа N):
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    System.Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
